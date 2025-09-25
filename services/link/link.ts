0e7d335b9573993d483c4d2107dee1ec07f45273
Rafael Maccari <rafagmaccari@gmail.com>
2025-09-25 00:21:26 -0300

    remoção de comentário

--- services/link/link.ts
+++ services/link/link.ts
@@ -8,28 +8,3 @@ export interface Link {
     createdAt?: string;
     project: Project;
 }
-/*
-import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
-import { Project } from "../project/project.entity";
-
-@Entity('link')
-export class Link {
-    @PrimaryGeneratedColumn()
-    code?: number;
-
-    @Column()
-    title?: string;
-
-    @Column()
-    url?: string;
-
-    @Column()
-    previewImageUrl?: string;
-
-    @CreateDateColumn()
-    createdAt?: string;
-
-    @ManyToOne(() => Project, project => project.links, {nullable: false})
-    @JoinColumn({ name: 'projectCode', referencedColumnName: 'code' })
-    project?: Project; 
-}*/
