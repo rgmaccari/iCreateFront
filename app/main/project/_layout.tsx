c035713cf69d7709db491b6e7dfacf6804b19e98
Rafael Maccari <rafagmaccari@gmail.com>
2025-10-06 00:43:53 -0300

    passando as rotas para o layout corretamente

--- app/main/project/_layout.tsx
+++ app/main/project/_layout.tsx
@@ -1,4 +1,16 @@
 import { Stack } from 'expo-router';
+import { PaperProvider } from 'react-native-paper';
+
 export default function ProjectLayout() {
-    return <Stack screenOptions={{ headerShown: false }}><Stack.Screen name="all-projects-screen" options={{ title: 'Projetos' }} /></Stack>;
+    return (
+        <PaperProvider>
+            <Stack screenOptions={{ headerShown: false }}>
+                <Stack.Screen name="all-projects-screen" options={{ title: 'Projetos' }} />
+                <Stack.Screen name="project" options={{ headerShown: false }} />
+                <Stack.Screen name="images-screen" options={{ headerShown: false }} />
+                <Stack.Screen name="links-screen" options={{ headerShown: false }} />
+                <Stack.Screen name="ai-features" options={{ headerShown: false }} />
+            </Stack>
+        </PaperProvider>
+    );
 }
