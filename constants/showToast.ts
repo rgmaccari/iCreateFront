ca7757ddf1dfd632c40b1acd774770e3926ef26b
Rafael Maccari <rafagmaccari@gmail.com>
2025-10-14 23:26:59 -0300

    show toast global

---
+++ constants/showToast.ts
@@ -0,0 +1,12 @@
+import Toast from "react-native-toast-message";
+
+export const showToast = (type: "success" | "error", text1: string, text2?: string) => {
+    Toast.show({
+        type,
+        text1,
+        text2,
+        visibilityTime: 4000,
+        autoHide: true,
+        position: "top",
+    });
+};
